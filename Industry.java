ecd01c8e00481904d55b6648434e3d115462c00c
VarshithaNR <rakshuvarshu216@gmail.com>
2026-04-22 22:45:14 +0530

    Array

---
+++ Industry.java
@@ -0,0 +1,159 @@
+class Industry {
+    public static void main(String[] args) {
+
+        byte workers = 120;
+        short branches = 15;
+        int revenue = 5000000;
+        long turnover = 100000000L;
+        float growthRate = 10.5f;
+        double profit = 2500000.75;
+        char grade = 'A';
+        boolean isActive = true;
+
+        System.out.println("Industry data stored");
+    }
+}
+
+class Pen {
+    public static void main(String[] args) {
+
+        byte quantity = 20;
+        short price = 50;
+        int totalSold = 1000;
+        long barcode = 1234567890L;
+        float length = 14.5f;
+        double weight = 9.75;
+        char colorCode = 'B';
+        boolean isRefillable = true;
+
+        System.out.println("Pen data stored");
+    }
+}
+
+class Chair {
+    public static void main(String[] args) {
+
+        byte legs = 4;
+        short price = 1500;
+        int stock = 200;
+        long productId = 987654321L;
+        float height = 3.2f;
+        double weight = 7.50;
+        char quality = 'A';
+        boolean isCushioned = true;
+
+        System.out.println("Chair data stored");
+    }
+}
+
+class Wallet {
+    public static void main(String[] args) {
+
+        byte compartments = 5;
+        short price = 800;
+        int stock = 150;
+        long serialNumber = 456789123L;
+        float length = 10.5f;
+        double weight = 0.75;
+        char materialGrade = 'B';
+        boolean isLeather = true;
+
+        System.out.println("Wallet data stored");
+    }
+}
+
+class Fan {
+    public static void main(String[] args) {
+
+        byte blades = 3;
+        short price = 2500;
+        int stock = 80;
+        long modelNumber = 1122334455L;
+        float speed = 3.5f;
+        double powerConsumption = 75.5;
+        char rating = 'A';
+        boolean isRemoteControlled = true;
+
+        System.out.println("Fan data stored");
+    }
+}
+
+class LED {
+    public static void main(String[] args) {
+
+        byte size = 32;
+        short price = 15000;
+        int stock = 40;
+        long serialNumber = 9988776655L;
+        float screenSize = 32.5f;
+        double powerUsage = 120.75;
+        char energyRating = 'A';
+        boolean isSmartTV = true;
+
+        System.out.println("LED data stored");
+    }
+}
+
+class Paper {
+    public static void main(String[] args) {
+
+        byte thickness = 2;
+        short price = 200;
+        int sheets = 500;
+        long batchNumber = 6677889900L;
+        float length = 29.7f;
+        double weight = 1.25;
+        char quality = 'A';
+        boolean isA4Size = true;
+
+        System.out.println("Paper data stored");
+    }
+}
+
+class Laptop {
+    public static void main(String[] args) {
+
+        byte ram = 8;
+        short price = 5500;
+        int storage = 512;
+        long serialNumber = 1231231231L;
+        float screenSize = 15.6f;
+        double processorSpeed = 3.75;
+        char grade = 'A';
+        boolean isTouchScreen = false;
+
+        System.out.println("Laptop data stored");
+    }
+}
+
+class Bag {
+    public static void main(String[] args) {
+
+        byte compartments = 4;
+        short price = 1200;
+        int stock = 300;
+        long productCode = 3213213210L;
+        float weight = 1.5f;
+        double capacity = 25.75;
+        char size = 'M';
+        boolean isWaterproof = true;
+
+        System.out.println("Bag data stored");
+    }
+}
+
+class Bottle {
+    public static void main(String[] args) {
+
+        byte capacity = 2;
+        short price = 300;
+        int stock = 100;
+        long barcode = 555666777L;
+        float height = 25.5f;
+        double weight = 0.85;
+        char materialGrade = 'A';
+        boolean isReusable = true;
+
+        System.out.println("Bottle data stored");
+    }
+}
---
+++ StudentInfo.java
@@ -0,0 +1,46 @@
+class StudentInfo{
+	public static void main(String[] args){
+		System.out.println("My family info");
+		System.out.println("Father:Ravi");
+		System.out.println("Mother:Sudha");
+		System.out.println("Sister:Rakshitha");
+	}
+}
+
+class EducationInfo{
+	public static void main(String[] args){
+		System.out.println("My Education info");
+		System.out.println("10th:CBSE,86.6");
+		System.out.println("12th:91.3");
+		System.out.println("BE:CSE,9.19");
+	}
+}
+
+class FriendsInfo{
+	public static void main(String[] args){
+		System.out.println("My friends info");
+		System.out.println("Varshu");
+		System.out.println("Rakshu");
+		System.out.println("Sindhu");
+		System.out.println("Tanushree");
+		System.out.println("sudha,ravi");
+	}
+}
+
+class ProudInfo{
+	public static void main(String[] args){
+		System.out.println("My Proud info");
+		System.out.println("Surviving in college");
+		System.out.println("doing my major & minor projetct alone");
+		System.out.println("not killing anyone");
+	}
+}
+
+class MovieInfo{
+	public static void main(String[] args){
+		System.out.println("My Movie info");
+		System.out.println("Asvin");
+		System.out.println("It has both historical and emotional mix of both");
+		System.out.println("The name is very unique which made to watcth");
+	}
+}
